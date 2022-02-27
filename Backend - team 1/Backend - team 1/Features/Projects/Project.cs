62f6aba18c3c447c4e0ad707b9ada799d7a84d97
Andrei Dobre <andrei.dobr3@gmail.com>
2022-02-27 16:32:13 +0200

    Revert "Revert "Merge pull request #17 from dsc-upt/Fabian""
    This reverts commit 643220bd58a1c5283989153b187678e79dd76715.

--- Backend - team 1/Backend - team 1/Features/Projects/Project.cs
+++ Backend - team 1/Backend - team 1/Features/Projects/Project.cs
@@ -1,6 +1,19 @@
+using Backend___team_1.Base.Entities;
+using Backend___team_1.Features.Clients;
+using Backend___team_1.Features.Teams;
+using Backend___team_1.Features.UserProfiles;
+
 namespace Backend___team_1.Features.Projects;
 
-public class Project
+public class Project : Entity
 {
+    public string Name { get; set; }
+
+    public UserProfile Manager { get; set; }
     
+    public string Status { get; set; }
+    
+    public Client Client { get; set; }
+    
+    public ICollection<Team> Teams { get; set; }
 }

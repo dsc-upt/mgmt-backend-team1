b7e4ddd536647ea00e20394137a129715a2f0f78
Fabian12Florin <ffabian08642@gmail.com>
2022-02-27 17:06:42 +0200

    Added Client Request & Response View

---
+++ Backend - team 1/Backend - team 1/Features/Clients/ClientRequestView.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Backend___team_1.Features.Clients;
+
+public class ClientRequestView
+{
+    [Required]
+    public string Name { get; set; }
+    
+    [Required]
+    public string ContactPersonId { get; set; }
+    
+    [Required]
+    [EmailAddress]
+    public string Email { get; set; }
+    
+    [Required]
+    [Phone]
+    public string Phone { get; set; }
+}
---
+++ Backend - team 1/Backend - team 1/Features/Clients/ClientResponseView.cs
@@ -0,0 +1,9 @@
+﻿namespace Backend___team_1.Features.Clients;
+
+public class ClientResponseView
+{
+    public string Id { get; set; }
+    public string Name { get; set; }
+    public string Email { get; set; }
+    public string Phone { get; set; }
+}

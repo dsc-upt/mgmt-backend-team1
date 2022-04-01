0a5f59ef09852c75838c4f4158d83d087cec917b
Andrei Dobre <andrei.dobr3@gmail.com>
2022-04-01 15:28:42 +0300

    Added FilesController

--- Backend - team 1/Backend - team 1/Base/Entities/Entity.cs
+++ Backend - team 1/Backend - team 1/Base/Entities/Entity.cs
@@ -2,7 +2,7 @@ namespace Backend___team_1.Base.Entities;
 
 public class Entity
 {
-    public string? Id { get; set; }
+    public string Id { get; set; }
     public DateTime Created { get; set; } = DateTime.Now;
     public DateTime Updated { get; set; }
 }
--- Backend - team 1/Backend - team 1/Base/Files/FileResponseView.cs
+++ Backend - team 1/Backend - team 1/Base/Files/FileResponseView.cs
@@ -1,15 +1,14 @@
-using Backend___team_1.Base.Entities;
-
 namespace Backend___team_1.Base.Files;
 
-public class FileModel : Entity
+public class FileResponseView
 {
+    public string Id { get; set; }
+    
     public string Name { get; set; }
     
     public string Path { get; set; }
     
     public string Extension { get; set; }
-
-    public long Size { get; set; }
     
+    public long Size { get; set; }
 }
